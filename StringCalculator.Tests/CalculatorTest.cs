b166013cb167305de9acb33cfbb142bec558b44d
Thanos Papathanasiou <ThanosPapathanasiou@hotmail.com>
2015-05-27 21:30:03 +0300

    String Calculator handles empty string and single number input

---
+++ StringCalculator.Tests/CalculatorTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace StringCalculator.Tests {
+    [TestFixture]
+    public class CalculatorTest
+    {
+        private Calculator calc;
+
+        [SetUp]
+        public void NewCalc()
+        {
+             calc = new Calculator();
+        }
+
+        [Test]
+        public void Add_Given_EmptyString_Returns_Zero()
+        {
+            int result = calc.Add("");
+            Assert.AreEqual(result, 0);
+        }
+
+        [TestCase("1",1)]
+        [TestCase("99", 99)]
+        public void Add_Given_OneNumber_Returns_Number(string numbers, int expected)
+        {
+            int result = calc.Add(numbers);
+            Assert.AreEqual(result, expected);
+        }
+
+    }
+}
---
+++ StringCalculator/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator {
+    public class Calculator {
+        public int Add(string numbers)
+        {
+            if (numbers == "")
+                return 0;
+
+            return Int32.Parse(numbers);
+        }
+    }
+}
